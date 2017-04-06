3868c7bab1278414345d1fc3cf63a897f3025f76
paul604 <paulorhon@live.fr>
2017-04-06 16:26:07 +0200

    WIP tab

--- js/main.js
+++ js/main.js
@@ -41,8 +41,7 @@ function getCommune(commune, response){
 function getImg(ville, nbImg){//https://www.flickr.com/services/api/flickr.places.find.htm    https://www.flickr.com/services/api/flickr.photos.search.html
 
     var HtmlPhoto = $("#photos")
-    var HtmlTab = $("#tableau");
-    HtmlPhoto.html("");
+    var HtmlTab = $("table").DataTable();
 
     var outData='method=flickr.places.find&api_key=f3edd30f7b0c323e51d713ed10145b26&format=json&query='+ville;
     $.ajax({//get id ville
@@ -61,14 +60,16 @@ function getImg(ville, nbImg){//https://www.flickr.com/services/api/flickr.place
                 jsonp : 'jsoncallback',
                 data : dataIdImg,
                 success : function(outImg, statutImg){
+
+                    //clear afich
+                    HtmlPhoto.html("");
+                    // HtmlTab.row(".odd").remove().draw( false );
+                    // HtmlTab.row(".even").remove().draw( false );
+                    // HtmlTab.fnDeleteRow();
+                    $("tbody").html("");
+
                     // console.log(outImg.photos.photo);//https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
                     var arrayObjImg = outImg.photos.photo;
-
-                    if(arrayObjImg.length==0){
-                        //TODO modal
-
-                        return;
-                    }
 
                     $.each(arrayObjImg, function(i,val){
                         if(i>=nbImg){
@@ -82,9 +83,21 @@ function getImg(ville, nbImg){//https://www.flickr.com/services/api/flickr.place
                             jsonp : 'jsoncallback',
                             data : idImg,
                             success : function(outInfoImg, statutIImg){
-                                console.log(outInfoImg.photo);
+
+                                outInfoImg=outInfoImg.photo;
+                                console.log(outInfoImg);
                                 var img = "<img src=\"https://farm"+val.farm+".staticflickr.com/"+val.server+"/"+val.id+"_"+val.secret+"_q.jpg)\" alt=\""+val.title+"\" />";
+
+                                //add list img
                                 HtmlPhoto.append(img);
+
+                                //add dan le tab
+                                HtmlTab.row.add( [
+                                    img,
+                                    outInfoImg.title._content,
+                                    outInfoImg.dates.taken,
+                                    outInfoImg.owner.realname
+                                ] ).draw( false );
                             }
                         });
                     });
